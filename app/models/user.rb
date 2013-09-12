b3c9a3d5b625442466baab455e23e48a80e8dac4
Salman Vajid <s@vajid.com>
2013-09-12 00:44:15 -0400

    Adding user names

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,10 @@
 class User < ActiveRecord::Base
+	has_many :reviews
+	has_many :products, :through => :reviews
+
+	validates_presence_of :name
 
 	has_secure_password
 	validates_presence_of :password, :on => :create
-
-	has_many :reviews
-	has_many :products, :through => :reviews
 end
 
